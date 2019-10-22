b84028e3a94d20afbf25c4853a440da1a4d4de0b
GiovanniCodes <33938554+GiovanniCodes@users.noreply.github.com>
2019-10-22 17:21:42 -0400

    Add files via upload

--- Assignment2_test_doubles.py
+++ Assignment2_test_doubles.py
@@ -2,16 +2,17 @@ import unittest
 from datetime import datetime
 
 from databaseFunc import databaseNeeds
+from dataBaseFuncTest import databaseNeedStub
 # from Assignment2 import shortestDistance, retirement
 
 
 class TestRetire(unittest.TestCase):
     def test_Mock_Connection(self):
-        self.assertFalse(databaseNeeds.connection("gg"))
+        self.assertTrue(databaseNeedStub.connection())
 
     def test_Stub_setUp(self):
         # Stub to return 20 if no data to print
-        self.assertAlmostEqual(databaseNeeds.printData({}),20);
+        self.assertAlmostEqual(databaseNeedStub.printStubData({'x':1}),10);
 
     def test_retire_insert(self):
         self.assertTrue({
@@ -28,11 +29,11 @@ class TestRetire(unittest.TestCase):
 
 class TestShort(unittest.TestCase):
     def test_Mock_Connection(self):
-        self.assertFalse(databaseNeeds.connection("mongodb://localhost:270/"))
+        self.assertTrue(databaseNeedStub.connection())
 
     def test_Stub_setUp(self):
         # Stub to return 20 if no data to print
-        self.assertNotEqual(databaseNeeds.printData({}),10);
+        self.assertNotEqual(databaseNeedStub.printStubData({}),20);
     def test_insert(self):
         self.assertTrue( {'x1': 3, 'x2': 4, 'y1': 8,'y2': 2,'timestamp': 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.now()) })
 
